b1f036c3825b57099fa1b91631451d0d5b7a0e36
Mohamed <hamom5075@gmail.com>
2025-11-27 14:51:11 +0200

    first commit of the project

---
+++ Tourism_Proj/Tourism.PLL/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Tourism_Proj.Tourism.PLL.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
